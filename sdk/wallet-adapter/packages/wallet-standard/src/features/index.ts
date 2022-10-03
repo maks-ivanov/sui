f6d900130d97c7d58f96786f2b60531363db584c
Jordan Gensler <jordan@mystenlabs.com>
2022-10-02 21:29:30 -0700

    Add header comments

--- sdk/wallet-adapter/packages/wallet-standard/src/features/index.ts
+++ sdk/wallet-adapter/packages/wallet-standard/src/features/index.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import type { WalletWithFeatures } from '@wallet-standard/standard';
 import type { SuiSignAndExecuteTransactionFeature } from './suiSignAndExecuteTransaction';
 import type { SuiSignTransactionFeature } from './suiSignTransaction';
